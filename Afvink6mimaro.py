4139c3745f49ef3e43a654717efaa6f262d20365
mathijs159 <mathijs159@users.noreply.github.com>
2015-03-16 15:31:36 +0100

    nieuwe functie aangemaakt test

--- Afvink6mimaro.py
+++ Afvink6mimaro.py
@@ -13,4 +13,8 @@ def bepaalGCpercentage (sequentie):
     gcPercentage = float(gcCount) / float(totalCount)
     return(float(gcPercentage)*100)
 
+def maakHTML (bepaalGCpercentage):
+    #maakt een HTML bestand met de sequentie en berekende GC percentage
+    
+
 main()
