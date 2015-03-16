5c52617877858a98c8fa4f754d2d9e094b609706
mikeduiven <mike.aaldering098@gmail.com>
2015-03-16 14:29:29 +0100

    tester

--- deel Mike.py
+++ deel Mike.py
@@ -1,15 +1,20 @@
 Hallo iedereen! dit is mijn mooie code
+kijken wat er gebeurd
 nu zeker niet
 
 
 Rowan test
 Rowan test 2
-Rowan 1
-Rowan 2
-Rowan 3
-Rowan 4
-Rowan 5
-Rowan 6
-Rowan 7
-Rowan 8
-Rowan 9
+mike test 1
+mike test 2
+mike test 3
+mike test 4
+mike test 5
+mike test 6
+
+
+
+
+
+
+lalala test
