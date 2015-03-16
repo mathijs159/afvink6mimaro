56db3f62e016d49fa19a6c57c5ef99d9f3dfcd90
mathijs159 <mathijs159@users.noreply.github.com>
2015-03-16 14:25:32 +0100

    hoera

--- deel Mike.py
+++ deel Mike.py
@@ -1,5 +1,12 @@
-Hallo iedereen! dit is mijn mooie code
-nu zeker niet
 
-
-Rowan test
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+# Explicit paths specified without -i or -o; assuming --only paths...
+# On branch master
+# Your branch is behind 'origin/master' by 1 commit, and can be fast-forwarded.
+#   (use "git pull" to update your local branch)
+#
+# Changes to be committed:
+#	modified:   deel Mike.py
+#
+fantastische verandering
